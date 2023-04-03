ebbed55925955909f2a277b37546c3d2ed706521
Lucas NG <3543285+lucasng-dev@users.noreply.github.com>
2023-04-03 15:37:14 -0300

    feat: add editor custom module

---
+++ lib/modules/_editor/init.zsh
@@ -0,0 +1,19 @@
+if [[ -n "$DISPLAY" ]]; then
+  # gui mode
+  if command -v gnome-text-editor &>/dev/null; then
+    export EDITOR="$(command -v gnome-text-editor) -s"
+    export VISUAL="$EDITOR"
+  elif command -v gedit &>/dev/null; then
+    export EDITOR="$(command -v gedit) -s"
+    export VISUAL="$EDITOR"
+  fi
+else
+  # console mode
+  if command -v nano &>/dev/null; then
+    export EDITOR="$(command -v nano)"
+    export VISUAL="$EDITOR"
+  elif command -v vim &>/dev/null; then
+    export EDITOR="$(command -v vim)"
+    export VISUAL="$EDITOR"
+  fi
+fi
