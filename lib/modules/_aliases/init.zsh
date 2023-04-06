4359f898bd43f8eff4a801562a5422a9d5780687
Lucas NG <3543285+lucasng-dev@users.noreply.github.com>
2023-04-06 20:04:04 -0300

    feat: simplify upgrade alias

--- lib/modules/_aliases/init.zsh
+++ lib/modules/_aliases/init.zsh
@@ -2,15 +2,14 @@ alias c='clear'
 
 alias @zshup='git -C "$ZDOTDIR/.." pull >/dev/null; zsh "$ZDOTDIR/../install.zsh"'
 
-__upgrade_cmds+=()
-if command -v dnf &>/dev/null; then
-  __upgrade_cmds+=('echo && echo "### DNF ###" && sudo dnf upgrade && sudo dnf autoremove -y;')
-elif command -v apt-get &>/dev/null; then
-  __upgrade_cmds+=('echo && echo "### APT ###" && sudo apt-get update && sudo apt-get upgrade && sudo apt-get autoremove -y;')
-fi
-if command -v flatpak &>/dev/null; then
-  __upgrade_cmds+=('echo && echo "### FLATPAK ###" && sudo flatpak update && sudo flatpak uninstall --unused -y;')
-fi
-__upgrade_cmds="${__upgrade_cmds[@]}"
-alias @upgrade="$__upgrade_cmds"
-unset __upgrade_cmds
+function __upgrade() {
+  if command -v dnf &>/dev/null; then
+    echo && echo "### DNF ###" && sudo dnf upgrade && sudo dnf autoremove -y
+  elif command -v apt-get &>/dev/null; then
+    echo && echo "### APT ###" && sudo apt-get update && sudo apt-get upgrade && sudo apt-get autoremove -y
+  fi
+  if command -v flatpak &>/dev/null; then
+    echo && echo "### FLATPAK ###" && sudo flatpak update && sudo flatpak uninstall --unused -y
+  fi
+}
+alias @upgrade="__upgrade"
