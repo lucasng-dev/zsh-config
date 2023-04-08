3eb41ecddcb32f1496a7e346be1b5de4e77630fc
Lucas NG <3543285+lucasng-dev@users.noreply.github.com>
2023-04-08 20:06:25 -0300

    feat: add upgrade alias for pacman

--- lib/modules/_aliases/init.zsh
+++ lib/modules/_aliases/init.zsh
@@ -7,6 +7,8 @@ function __upgrade() {
     echo && echo "### DNF ###" && sudo dnf upgrade && sudo dnf autoremove -y
   elif command -v apt-get &>/dev/null; then
     echo && echo "### APT ###" && sudo apt-get update && sudo apt-get upgrade && sudo apt-get autoremove -y
+  elif command -v pacman &>/dev/null; then
+    echo && echo "### PACMAN ###" && sudo pacman -Syu && { sudo pacman -Qtdq | sudo pacman --noconfirm -Rns - &>/dev/null || true; }
   fi
   if command -v flatpak &>/dev/null; then
     echo && echo "### FLATPAK ###" && sudo flatpak update && sudo flatpak uninstall --unused -y
