86a6a90c9d94cf9ae192aec6993e3f601639e4a8
Lucas NG <lucasng.dev@gmail.com>
2023-04-13 18:21:37 -0300

    feat: update editor module

--- lib/modules/_editor/init.zsh
+++ lib/modules/_editor/init.zsh
@@ -1,19 +1,17 @@
+# console mode
+if command -v vim &>/dev/null; then
+  export EDITOR="$(command -v vim)"
+elif command -v nano &>/dev/null; then
+  export EDITOR="$(command -v nano)"
+fi
+
+# gui mode
 if [[ -n "$DISPLAY" ]]; then
-  # gui mode
   if command -v gnome-text-editor &>/dev/null; then
     export EDITOR="$(command -v gnome-text-editor) -s"
-    export VISUAL="$EDITOR"
   elif command -v gedit &>/dev/null; then
     export EDITOR="$(command -v gedit) -s"
-    export VISUAL="$EDITOR"
-  fi
-else
-  # console mode
-  if command -v vim &>/dev/null; then
-    export EDITOR="$(command -v vim)"
-    export VISUAL="$EDITOR"
-  elif command -v nano &>/dev/null; then
-    export EDITOR="$(command -v nano)"
-    export VISUAL="$EDITOR"
   fi
 fi
+
+export VISUAL="$EDITOR"
