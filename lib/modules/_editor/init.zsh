d7eb0bca2c8e31797931cee8afc487c62e060ff7
Lucas NG <3543285+lucasng-dev@users.noreply.github.com>
2023-04-15 12:27:17 -0300

    feat: upgrate editor module

--- lib/modules/_editor/init.zsh
+++ lib/modules/_editor/init.zsh
@@ -1,16 +1,16 @@
 # console mode
 if command -v vim &>/dev/null; then
-  export EDITOR="$(command -v vim)"
+  export EDITOR="vim"
 elif command -v nano &>/dev/null; then
-  export EDITOR="$(command -v nano)"
+  export EDITOR="nano"
 fi
 
 # gui mode
 if [[ -n "$DISPLAY" ]]; then
   if command -v gnome-text-editor &>/dev/null; then
-    export EDITOR="$(command -v gnome-text-editor) -s"
+    export EDITOR="gnome-text-editor -s"
   elif command -v gedit &>/dev/null; then
-    export EDITOR="$(command -v gedit) -s"
+    export EDITOR="gedit -s"
   fi
 fi
 
