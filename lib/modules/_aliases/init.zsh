b402f94228bb90b0b0e1c1bf6b5d03190cfcb99c
Lucas NG <3543285+lucasng-dev@users.noreply.github.com>
2023-05-12 15:57:22 -0300

    feat: update aliases/functions

--- lib/modules/_aliases/init.zsh
+++ lib/modules/_aliases/init.zsh
@@ -2,13 +2,13 @@ alias c='clear'
 
 alias @zshup='git -C "$ZDOTDIR/.." pull >/dev/null; zsh "$ZDOTDIR/../install.zsh"'
 
-function __upgrade() {
+function @upgrade() {
   if command -v rpm-ostree &>/dev/null; then
     echo && echo "### RPM-OSTREE ###" && rpm-ostree upgrade
   elif command -v dnf &>/dev/null; then
     echo && echo "### DNF ###" && sudo dnf upgrade && sudo dnf autoremove -y
-  elif command -v apt-get &>/dev/null; then
-    echo && echo "### APT ###" && sudo apt-get update && sudo apt-get upgrade && sudo apt-get autoremove -y
+  elif command -v apt &>/dev/null; then
+    echo && echo "### APT ###" && sudo apt update && sudo apt upgrade && sudo apt autoremove -y
   elif command -v pacman &>/dev/null; then
     echo && echo "### PACMAN ###" && sudo pacman -Syu && { sudo pacman -Qtdq | sudo pacman --noconfirm -Rns - &>/dev/null || true; }
   fi
@@ -16,4 +16,3 @@ function __upgrade() {
     echo && echo "### FLATPAK ###" && flatpak update && flatpak uninstall --unused -y
   fi
 }
-alias @upgrade="__upgrade"
--- lib/modules/_node/init.zsh
+++ lib/modules/_node/init.zsh
@@ -4,19 +4,17 @@ if [[ -s "$HOME/.nvs/nvs.sh" ]]; then
   fi
 
   if nvs -v &>/dev/null; then
-    function __nvs_use() {
+    function @node() {
       local version="${1:-lts}"
       if ! nvs use "$version" &>/dev/null; then
         nvs add "$version" && nvs use "$version"
       fi
     }
-
-    __nvs_use
-    nvs auto on &>/dev/null
-
-    alias @node='__nvs_use'
     alias @nodeup='nvs upgrade'
     alias @noderm='nvs rm'
     alias @nodels='nvs ls'
+
+    @node
+    nvs auto on &>/dev/null
   fi
 fi
