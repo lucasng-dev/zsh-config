46c72f004644f39ae36efa4345f8372ea080778b
Lucas NG <3543285+lucasng-dev@users.noreply.github.com>
2023-05-15 12:37:12 -0300

    feat: update commands

--- lib/modules/_aliases/init.zsh
+++ lib/modules/_aliases/init.zsh
@@ -4,7 +4,7 @@ alias @zshup='git -C "$ZDOTDIR/.." pull >/dev/null; zsh "$ZDOTDIR/../install.zsh
 
 function @upgrade() {
   if command -v rpm-ostree &>/dev/null; then
-    echo && echo "### RPM-OSTREE ###" && rpm-ostree upgrade
+    echo && echo "### RPM-OSTREE ###" && sudo rpm-ostree upgrade
   elif command -v dnf &>/dev/null; then
     echo && echo "### DNF ###" && sudo dnf upgrade && sudo dnf autoremove -y
   elif command -v apt &>/dev/null; then
@@ -13,6 +13,6 @@ function @upgrade() {
     echo && echo "### PACMAN ###" && sudo pacman -Syu && { sudo pacman -Qtdq | sudo pacman --noconfirm -Rns - &>/dev/null || true; }
   fi
   if command -v flatpak &>/dev/null; then
-    echo && echo "### FLATPAK ###" && flatpak update && flatpak uninstall --unused -y
+    echo && echo "### FLATPAK ###" && sudo flatpak update && sudo flatpak uninstall --unused -y
   fi
 }
